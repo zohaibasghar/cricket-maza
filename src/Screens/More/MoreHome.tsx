d97a11cf5fa97d71c55bc9e20b7bf113d267fde4
osamaa15 <osaift@gmail.com>
2024-05-31 19:24:42 +0500

    more screen inprogress

--- src/Screens/More/MoreHome.tsx
+++ src/Screens/More/MoreHome.tsx
@@ -1,15 +1,23 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { VStack, Text, Box } from "native-base";
 
 const MoreHome = () => {
   const nav = useNavigation();
 
   // nav.navigate("MoreStack", { screen: "Login" });
   return (
-    <View>
-      <Text>MoreHome</Text>
-    </View>
+    <Box w={"100%"} h={"100%"}>
+      <VStack space={10}>
+        <VStack justifyContent={"center"} alignItems="center" bg="primary.400" h={"40%"}>
+          <Text fontSize="5xl">Settings</Text>
+        </VStack>
+        <VStack justifyContent={"center"} alignItems="center" bg="primary.600" h={"20%"}>
+          <Text>asd</Text>
+        </VStack>
+      </VStack>
+    </Box>
   );
 };
 
